07f11a9849978357b51fd967cbe0c47c102d9949
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2023-07-04 14:51:55 +0200

    Reorganizing FNO conv layer

--- src/utils/layers.py
+++ src/utils/layers.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from utils.utilities import periodic_padding
 
@@ -41,3 +42,40 @@ class SeparableConv(nn.Module):
 	
 	def forward(self, x):
 		return self.convDepthwise(self.convSpatial(x))
+
+class SpectralConv2d(nn.Module):
+    """
+	Taken from FNO paper.
+    2D Fourier layer. It does FFT, linear transform, and Inverse FFT.    
+    """
+    def __init__(self, in_channels, out_channels, modes1, modes2):
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.modes1 = modes1 #Number of Fourier modes to multiply, at most floor(N/2) + 1
+        self.modes2 = modes2
+
+        self.scale = (1 / (in_channels * out_channels))
+        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+        self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+
+    def compl_mul2d(self, input_, weight):
+        # (batch, in_channel, x,y ), (in_channel, out_channel, x,y) -> (batch, out_channel, x,y)
+        return torch.einsum("bixy,ioxy->boxy",input_,weight)
+
+    def forward(self, x):
+        batchsize = x.shape[0]
+        #Compute Fourier coeffcients up to factor of e^(- something constant)
+        x_ft = torch.fft.rfft2(x)
+
+        # Multiply relevant Fourier modes
+        out_ft = torch.zeros(batchsize, self.out_channels,  x.size(-2), x.size(-1)//2 + 1, dtype=torch.cfloat, device=x.device)
+        out_ft[:, :, :self.modes1, :self.modes2] = self.compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
+        out_ft[:, :, -self.modes1:, :self.modes2] = self.compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
+
+        # spatial dimensions of the input and output activation maps is the same.
+
+        #Return to physical space
+        x = torch.fft.irfft2(out_ft, s=( x.size(-2), x.size(-1)))
+        return x
