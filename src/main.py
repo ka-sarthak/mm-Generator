db852a5073d1f8f9dcdc4755a21dd2c73629e3cf
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2023-07-04 14:16:27 +0200

    Bug fix: using assertion error instead of try

--- src/main.py
+++ src/main.py
@@ -1,18 +1,15 @@
 import sys
-from config_module import configLoad
-from train import train
-from inference import inference
+from utils.config_module import configLoad
+from model_train import train
+from model_inference import inference
 
 if __name__ == "__main__":
 	# initialize the config
 	configLoad("../config.yml")
-	
-	try:
-		if sys.argv[1] == "--train" or sys.argv[1] == "--t":
-			train()
-		elif sys.argv[1] == "--inference" or sys.argv[1] == "--i":
-			inference()
-		else:
-			raise(TypeError)
-	except TypeError:
-		print("Argument not found. Should be one of --train (--t) or --inference (--i).")
+
+	if sys.argv[1] == "--train" or sys.argv[1] == "--t":
+		train()
+	elif sys.argv[1] == "--inference" or sys.argv[1] == "--i":
+		inference()
+	else:
+		raise AssertionError("ArgumentNotFound: Should be --train (--t) or --inference (--i).")
