7e292f44266edffee73681acb101b3c81a88fb94
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2023-08-14 01:04:12 +0200

    Adding FT selected modes plot

--- src/utils/probe_fourier_modes.py
+++ src/utils/probe_fourier_modes.py
@@ -1,4 +1,6 @@
 import os 
+import numpy as np
+import matplotlib.pyplot as plt
 from utils.config_module import config
 from utils.postprocessing import plotAllChannels
 
@@ -27,7 +29,9 @@ class ProbeFourierModes():
         
     def stopCollection(self):
         self.collect = False
-        self._plot(rescaling=True)
+        self._createPath()
+        self._plotFT(rescaling=True)
+        self._plotSampledModesVsDominant(rescaling=True)
         print(f"Probe Stop :: probeFourierMode has data from {max(list(self.data.keys()))} epochs.")
         
     def collectData(self, x):
@@ -50,12 +54,46 @@ class ProbeFourierModes():
     def setData(self,data):
         self.data = data
         
-    def _plot(self,rescaling=False):
-        self._createPath()
+    def _plotFT(self,rescaling=False):
+        path = os.path.join(self.path,"plotFT","")
+        os.makedirs(path,exist_ok=True)
         for ep,ep_data in self.data.items():
             for layer,layer_data in ep_data.items():
-                img_name = self.path+"epoch_"+str(ep)+"_layer_"+str(layer)
+                img_name = path+"epoch_"+str(ep)+"_layer_"+str(layer)
                 plotAllChannels(data=layer_data,cmap="Greys",path_and_name=img_name,rescaling=rescaling)
+
+    def _plotSampledModesVsDominant(self,rescaling=False):
+        path = os.path.join(self.path,"sampledModesFT","")
+        os.makedirs(path,exist_ok=True)
+        
+        modes1 = config["model"]["FNO"]["modes1"]
+        modes2 = config["model"]["FNO"]["modes2"]
+        
+        for ep,ep_data in self.data.items():
+            for layer,layer_data in ep_data.items():
+                img_name = path+"epoch_"+str(ep)+"_layer_"+str(layer)
+                
+                cols = 6
+                rows = int(np.ceil(layer_data.shape[0]/cols))
+                fig = plt.figure(figsize=(cols*2,rows*2))
+                for i, channel in enumerate(layer_data):
+                    if rescaling:
+                        channel = (channel-np.min(channel))/(np.max(channel)-np.min(channel))
+                    
+                    windowSelection1 = channel[:modes1,:modes2].flatten()
+                    windowSelection2 = channel[-modes1:,:modes2].flatten()
+                    windowSelection = np.flip(np.sort(np.concatenate((windowSelection1,windowSelection2))))
+                    
+                    dominantModes = np.flip(np.sort(channel.flatten()))[:modes1*modes2*2]
+                    ax = plt.subplot(rows,cols,i+1)
+                    plt.plot(windowSelection,label="window selection")
+                    plt.plot(dominantModes,label="dominant modes")
+
+                handles, labels = ax.get_legend_handles_labels()
+                fig.legend(handles, labels, loc='lower right')
+                plt.tight_layout()
+                plt.savefig(f"{img_name}.png", format="png", dpi=500, bbox_inches=0, transparent=True)
+                plt.close()
 
     def _createPath(self):
         self.path = os.path.join(config["path"]["saveModel"],config["experiment"]["model"], config["experiment"]["generator"], config["experiment"]["name"],"FourierModesCalculation","")
